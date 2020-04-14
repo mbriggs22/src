8007c6068dbcfa67a01e0e52ef4a51c4a8e66573
mbriggs22 <62741675+mbriggs22@users.noreply.github.com>
2020-04-14 11:45:29 -0600

    Add files via upload

---
+++ GE01.java
@@ -0,0 +1,18 @@
+package searchandsort;
+
+import java.until.List;
+import java.util.Random;
+import java.util.Scanner;
+
+/** 
+ * This is a class containing a variety of search and sort algorithms. 
+ */
+
+public class searchandsort {
+    
+    private static final Random RNG = new Random();
+}
+
+public static ArrayList<Integer> randomlist(int size, int range) {
+    ArrayList<Integer> result = new ArrayList();
+}
